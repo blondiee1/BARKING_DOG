8afd99729beb2438d4e6a2b0e197bb50e0c5e62a
blondiee1 <103431926+blondiee1@users.noreply.github.com>
2022-09-04 15:45:16 -0300

    2do ejerc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean doggy = shouldWakeUp(true, 9);
+        System.out.println(doggy);
     }
+
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (barking == true && hourOfDay < 8 || hourOfDay > 22) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
